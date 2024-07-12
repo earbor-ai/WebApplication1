dabff968be7de4279d45d7f914cdef11b52b4f25
Lakshmi Anuhya Koduri <lakshmianuhyakoduri@gmail.com>
2024-07-12 12:43:45 +0530

    File Upload code added in html pages

---
+++ Pages/upload.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class UploadModel : PageModel
+    {
+        private readonly ILogger<UploadModel> _logger;
+
+        public UploadModel(ILogger<UploadModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public IFormFile File { get; set; }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (File != null && File.Length > 0)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Fileupload", File.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await File.CopyToAsync(stream);
+                }
+
+                Message = "File uploaded successfully!";
+            }
+
+            return Page();
+        }
+    }
+}
